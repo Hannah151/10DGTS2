b431094535eb20739b71502d01dfa4c4209d1ec8
Hannah151 <hannah.ludemann@kingsway.school.nz>
2024-09-18 10:26:07 +1200

    Add files via upload
    My first coding on Python

---
+++ Welcome.py
@@ -0,0 +1,11 @@
+# My first Python program
+# Author: Hannah Ludemann
+# Date: 2024-09-18
+# Version: 1.0
+
+
+# This is a comment. It starts with a #.
+# Print out a welcome message.
+print('Hello World!\n') # \n creates a line break
+print() # This creates a line break
+print('Congratulations on writing your first program.')
